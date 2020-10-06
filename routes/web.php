a3b31ad3740f0c9ae377bc1a9776e766f31c142b
vorreakboth.chhoeurn <vorreakboth.chhoeurn@allweb.com.kh>
2020-10-06 16:25:18 +0700

    homework1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/product', function (){
+    $products = DB::select('SELECT * FROM products');
+    foreach ($products as $product){
+        echo $product->name. "\t". $product->price. '<br>';
+    }
+});
+Route::get('/product/insert', function (){
+   DB::insert('INSERT INTO products (name, price) VALUES (?, ?)',['name4',60]);
+});
+Route::get('/product/{id}/delete', function ($id){
+    DB::delete('DELETE FROM products WHERE id = ?', [$id]);
+    return redirect('/product');
+});
+Route::get('/product/{id}/update', function ($id){
+    DB::update('UPDATE products SET name = ?, price = ? WHERE id = ?', ['name1 updated', 60, $id]);
+    return redirect('/product');
+});
